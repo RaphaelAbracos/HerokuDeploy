6d5757e9971af47195cd3fc96fcd050b9c1243f2
BRUNO LEIVA PIRES <bruno.leivapires2706@gmail.com>
2020-10-30 21:33:20 -0300

    Versão Junta DashBoard e Login 30/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { FormularioLoginComponent } from './Login/formulario-login/formulario-login.component';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './dashboard/menu/menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -20,19 +18,15 @@ import { MatMenuModule } from '@angular/material/menu';
 import { CadastroLembreteComponent } from './dashboard/cadastro-lembrete/cadastro-lembrete.component';
 import { routing } from './app.routing';
 import { ExcluirLembreteComponent } from './dashboard/excluir-lembrete/excluir-lembrete.component';
->>>>>>> 3d030ad513d523bfd185191f0b7f07e30363f05f
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    FormularioLoginComponent
-=======
+    FormularioLoginComponent,
     MenuComponent,
     DbContentComponent,
     CadastroLembreteComponent,
     ExcluirLembreteComponent,
->>>>>>> 3d030ad513d523bfd185191f0b7f07e30363f05f
   ],
   imports: [
     BrowserModule,
